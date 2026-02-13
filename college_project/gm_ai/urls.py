738fe864d29fa1419bd4daddc8d29c4521f3da4d
Nodirbek <ccnodirbekcc@gmail.com>
2026-02-13 12:44:05 +0500

    added auth

--- college_project/gm_ai/urls.py
+++ college_project/gm_ai/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path
+from django.contrib import admin
 from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('admin/', admin.site.urls),
+
+    # AUTH
+    path('login/', views.login_view, name='login'),
+    path('register/', views.register_view, name='register'),
+    path('logout/', views.logout_view, name='logout'),
 ]
--- college_project/gm_ai/views.py
+++ college_project/gm_ai/views.py
@@ -1,6 +1,54 @@
-from django.shortcuts import render
-
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
+from django.contrib import messages
 
 def home(request):
     return render(request, 'home.html')
+
+# REGISTER
+def register_view(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        email = request.POST["email"]
+        password = request.POST["password"]
+
+        if User.objects.filter(username=username).exists():
+            messages.error(request, "Username already exists")
+            return redirect("register")
+
+        user = User.objects.create_user(
+            username=username,
+            email=email,
+            password=password
+        )
+        user.save()
+
+        login(request, user)
+        return redirect("home")
+
+    return render(request, "register.html")
+
+
+# LOGIN
+def login_view(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect("home")
+        else:
+            messages.error(request, "Invalid username or password")
+            return redirect("login")
+
+    return render(request, "login.html")
+
+
+# LOGOUT
+def logout_view(request):
+    logout(request)
+    return redirect("home")
